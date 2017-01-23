89ba3c9441867991fc9121cb5d381c5fb7d5833e
Michael Chiang <michiang@ucsd.edu>
2017-01-23 12:42:04 -0800

    Controller for routing GET requests and events basic implementation

--- public/songs.js
+++ public/songs.js
@@ -1,1 +1,43 @@
+var youtunes = angular.module('youtunes', []);
+
+function mainController($scope, $http) {
+  $scope.formData = {};
+
+    // when landing on the page, get all todos and show them
+    $http.get('/api/list')
+        .success(function(data) {
+            $scope.list = data;
+            console.log(data);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+
+    // when submitting the add form, send the text to the node API
+    $scope.createTodo = function() {
+        $http.post('/api/list', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {};
+                // clear the form so our user is ready to enter another
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    // delete a todo after checking it
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/list/' + id)
+            .success(function(data) {
+                $scope.list = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+}
 

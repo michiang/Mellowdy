859cc3e079a738bb65d29b6c11eb56f91cf7da67
Michael Chiang <michiang@ucsd.edu>
2017-01-24 11:01:00 -0800

    iFrame Player API

--- public/songs.js
+++ public/songs.js
@@ -39,7 +39,39 @@ function mainController($scope, $http) {
             });
     };
 
-
+     $scope.search = function (isNewQuery) {
+      $scope.loading = true;
+      return $http.get('https://www.googleapis.com/youtube/v3/search', {
+        params: {
+          key: 'AIzaSyAaVxe2e6AbU3FD2pKTQh1_AySRHC1NY8I',
+          type: 'video',
+          maxResults: '10',
+          pageToken: isNewQuery ? '' : $scope.nextPageToken,
+          part: 'id,snippet',
+          fields: 'items/id,items/snippet/title,items/snippet/description,items/snippet/thumbnails/default,items/snippet/channelTitle,nextPageToken',
+          q: this.query
+        }
+      })
+      .success( function (data) {
+        if (data.items.length === 0) {
+          $scope.label = 'No results were found!';
+        }
+        VideosService.listResults(data, $scope.nextPageToken && !isNewQuery);
+        $scope.nextPageToken = data.nextPageToken;
+        $log.info(data);
+        $scope.loading = false;
+      })
+      .error( function (e) {
+        $log.info('Search error: ', e);
+        $scope.loading = false;
+      })
+      ;
+    };
 
 }
+
+
+
+
+
 
